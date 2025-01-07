78c155f6d5c2078a217c51b862bfaa2ad69c33aa
fengqi <362254883@qq.com>
2025-01-07 10:12:52 +0800

    1. new font-style crate

---
+++ font-style/src/main.rs
@@ -0,0 +1,48 @@
+use floem::keyboard::{Key, NamedKey};
+use floem::prelude::*;
+use floem::style::{AlignItems, JustifyContent, StyleValue};
+use floem::views::container;
+
+fn app_view() -> impl IntoView {
+    // Color::rgba8(0, 0, 0, 120)
+    let view = v_stack((
+        container(label("{", None, Some(Color::parse("#C5E1C5").unwrap()))).style(|x| x.width(10.0))
+        , container(label("{", Some(Color::parse("#C5E1C5").unwrap()), None)).style(|x| x.width(10.0))
+        , container(label("{", Some(Color::parse("#98FB98").unwrap()), None)).style(|x| x.width(10.0))
+        , container(label("{", None, Some(Color::parse("#B7E1CD").unwrap())).style(|x| x.width(10.0))).style(|x| x.width(10.0))
+        , container(label("    println!(\"abc\")", None, None)).style(|x| x.width(100.0))
+    ));
+
+    let id = view.id();
+    view.on_key_up(
+        Key::Named(NamedKey::F11),
+        |m| m.is_empty(),
+        move |_| id.inspect(),
+    )
+}
+
+fn main() {
+    floem::launch(app_view);
+}
+
+fn label(text: &str, color: Option<Color>, bg_color: Option<Color>) -> impl View {
+    static_label(text).style(move |style| {
+        let style = style
+            .height(23.0)
+            .font_size(13.0)
+            .padding_horiz(4.0)
+            .font_family(StyleValue::Val("JetBrains Mono".to_string()))
+            .align_items(AlignItems::Center)
+            .justify_content(JustifyContent::FlexEnd);
+        let style = if let Some(color) = color {
+            style.color(color)
+        } else {
+            style
+        };
+        if let Some(bg) = bg_color {
+            style.background(bg)
+        } else {
+            style
+        }
+    })
+}
